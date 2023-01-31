7985c227e81382fa857eee052ae5fd97d52b97ae
hannahgarciaa <109315717+hannahgarciaa@users.noreply.github.com>
2023-01-31 15:00:55 -0600

    Update littleprofessor.py

--- littleprofessor.py
+++ littleprofessor.py
@@ -2,19 +2,26 @@ import random
 # rejects level of 0 
 # rejects level of 4 
 # rejects level of "one" :) 
-# accepts valid level :) 
+# accepts valid level 
 # generates addition problems using 0-9 level 1
 # generates addition problems using 10-99 level 2
 # generates addition problems using 100-999 level3
-# generates 10 problems before exiting
-# displays EEE when answer is incorrect 
-# shows solution after 3 incorrect attempts
+
+
 mylist = ["Correcto!", "Muy Bien!", "Sigue asi!"]
 
+
+while True:
+    cal = input("Operacion en signo que desea realizar?: ")
+    if cal in ("+", "-"):
+        break
+    print("Intenta de nuevo")
+    continue
 while True:
     nivel= input("Nivel: ")
     if nivel not in ["1", "2", "3"]:
-        break
+      print("Solo hay tres niveles. Reinicia el juego")
+      break
 
     while True:
         for _ in range(10):
@@ -28,12 +35,23 @@ while True:
              x = random.randint(100, 999)
              y = random.randint(100, 999)
 
-        #print()
-        answer = input(f"{x} + {y} = ")
-        if answer == str(x + y):
-          print(random.choice(mylist))
+
+        if cal == "+":
+          answer = input(f"{x} + {y} = ")
+          if answer == str(x + y):
+            print(random.choice(mylist))
+          else:
+            print("Incorrecto. Comienza de nuevo")
+            break
+
+        elif cal == "-":
+          res = input(f"{x} - {y} = ")
+          if res == str(x - y):
+            print(random.choice(mylist))
+          else:
+            print("Incorrecto. Comienza de nuevo")
+            break
         else:
-          print("Incorrecto. Comienza de nuevo")
           break
 
 
